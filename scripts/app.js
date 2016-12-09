7e8ca664512cf05e79630d750f250a2a71353ba9
c00z <zcusimano@gmail.com>
2016-12-08 20:24:58 -0800

    Added trump images

--- scripts/app.js
+++ scripts/app.js
@@ -4,22 +4,11 @@ var boxIndex = $('.box').index();
 
 $(document).ready(function() {
 
-// var boxNum = $(".box").length
-// // for (var i = 1; i <= boxNum; i++) {
-//   $('#p' + i).on('click', function() {
-  // for (var i = 1; i <= boxNum; i++) {
   $(".box").on("click", function() {
-    $(this).addClass("bombbox")
+    $(this).addClass("emptybox")
     console.log(this)
   })
-  // $(".box").addClass('.emptybox');
-//   })
-// }
-// $('.box').each(function(element) {
-//   $(element).on('click', function() {
-//     console.log('test')
-//   });
-//   });
+
 
 
 });
