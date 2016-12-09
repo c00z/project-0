3bd2ec2f783abfb3bdf519bd33c2dd85fed300d8
c00z <zcusimano@gmail.com>
2016-12-08 22:50:12 -0800

    added timer and page refresh

--- scripts/app.js
+++ scripts/app.js
@@ -5,9 +5,28 @@ var boxIndex = $('.box').index();
 $(document).ready(function() {
 
   $(".box").on("click", function() {
-    $(this).addClass("emptybox")
+    $(this).addClass("bombbox")
     console.log(this)
   })
+
+//Timer
+var count=5;
+var counter=setInterval(timer, 1000); //will run every 1 second
+
+function timer() {
+  count=count-1;
+  if (count < 0)
+  {
+  clearInterval(counter);
+     location.reload("emptybox");
+     return;
+  }
+
+  document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling
+
+
+
+}
 
 
 
