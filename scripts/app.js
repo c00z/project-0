e7d73729e84d131ecdcdf30a55ef840df7a6a4f2
c00z <zcusimano@gmail.com>
2016-12-12 21:41:17 -0800

    Final game play, win logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,26 @@
-var boxIndex = $('.box').index();
 var bombArray = [];
-var selections = [];
-var score = 0;
 var hasWon = false;
-
 var heartBeat = $("#heartBeat");
-
 
 $(document).ready(function() {
 
-  $(".box").on("click", function() {
-    if($(this).hasClass("bombbox")) {
+  $('#board').on('click', '.box', function(evt) {
+    var $element = $(evt.target);
+    var id = $element.attr('id');
+    var index = bombArray.indexOf(id);
+
+    if (index > -1) {
+      $element.addClass("bombbox");
+      bombArray.splice(index, 1);
     } else {
-    $(this).addClass("emptybox")
-  }
-  })
+      $element.addClass("emptybox");
+    }
+
+    if (!bombArray.length) {
+      hasWon = true;
+      alert('YOU DEFUSED THE BOMBS!');
+    }
+  });
 
 //array game board
 var gameBoard =
@@ -24,110 +30,101 @@ var gameBoard =
   'p19', 'p20', 'p21', 'p22', 'p23', 'p24',
   'p25', 'p26', 'p27', 'p28', 'p29', 'p30'];
 
-//Get random number, assign to bombArray & loop through to place randomly on board
-function createBoard () {
-for (var i = 0; i < 6; i++) {
-  bombArray.forEach(function(element) {
-    $('#' + element).click(function() {
-    $(this).addClass("bombbox");
-    })
-})
-  bombArray.push(gameBoard[(Math.floor(Math.random()*(30-1)+1))])
-  console.log(bombArray)
+  //Get random number, assign to bombArray & loop through to place randomly on board
+  function createBoard () {
+    bombArray = new Array(6).fill().map(() => {
+      return gameBoard[(Math.floor(Math.random()*(30-1)+1))];
+    });
+
+    console.log(bombArray)
+    return bombArray;
+  };
+
+  createBoard();
+
+  // var hasBeenClicked = false;
+  // jQuery('bombox').click(function () {
+  //     hasBeenClicked = true;
+  // });
+  //
+  // if (hasBeenClicked) {
+  //     $('.header').html(`You have won`);
+  // }
+
+  //
+  // $('.bombox').click(function(){
+  //   $(this).data('clicked', true);
+  //
+  // if($('.bombbox').data('clicked')) {
+  //     $('.header').html(`You have won`);
+  // // } else {
+  // //     //run function2
+  // }
+
+  // });
+  //
+  // if ($(".bombbox").on("click", function() {
+  //   // var selections = 0;
+  //   selections ++;
+  //   return;
+  //  $("#userscore").css("User Score: " + selections);
+  // })
+  // console.log(selections)
+
+  //Checks for win scenario in bombArray
+  // function checkWin(selections) {
+  //   var ret = false;
+  // bombArray.forEach(function(combo){
+  //     var count = 0;
+  //     combo.forEach(function(num) {
+  //       var match = selections.indexOf(num) > -1;
+  //       if (match) {
+  //         count++;
+  //       }
+  //     });
+  // //if greater than 5, considered win
+  //     if (count > 5) {
+  //       ret = true;
+  //     }
+  //   });
+  //   return ret;
+  // }
+  //
+  // console.log(selections)
+
+  //TIMER
+  var count = 0;
+  $(".button").on('click', function(){
+     var count = 11;
+     setInterval(function() {
+       count--;
+        if (count >= 0) {
+           span = $(".count");
+           span.innerHTML = count;
+           console.log(count)
+        }
+        if (count === 0) {
+           alert('YOU JUST WENT BOOOOOOM!');
+           clearInterval(count);
+           location.reload("#board");
+           return;
+         }
+         document.getElementById("timer").innerHTML=count + " secs";
+       }, 1000);
+  });
+
+  //cover div
+  //
+  // $("#dimScreen").on("click", function() {
+  //     $(".hiddenDiv").css({
+  //       height: "0px"
+  //     }, 1000 ); // how long the animation should be
+  //   });
+
+
+  // audio
+
+  function playHeart() {
+    heartBeat.play();
   }
-  return bombArray;
-};
-
-createBoard();
-
-
-
-// var hasBeenClicked = false;
-// jQuery('bombox').click(function () {
-//     hasBeenClicked = true;
-// });
-//
-// if (hasBeenClicked) {
-//     $('.header').html(`You have won`);
-// }
-
-//
-// $('.bombox').click(function(){
-//   $(this).data('clicked', true);
-//
-// if($('.bombbox').data('clicked')) {
-//     $('.header').html(`You have won`);
-// // } else {
-// //     //run function2
-// }
-
-// });
-//
-// if ($(".bombbox").on("click", function() {
-//   // var selections = 0;
-//   selections ++;
-//   return;
-//  $("#userscore").css("User Score: " + selections);
-// })
-// console.log(selections)
-
-//Checks for win scenario in bombArray
-// function checkWin(selections) {
-//   var ret = false;
-// bombArray.forEach(function(combo){
-//     var count = 0;
-//     combo.forEach(function(num) {
-//       var match = selections.indexOf(num) > -1;
-//       if (match) {
-//         count++;
-//       }
-//     });
-// //if greater than 5, considered win
-//     if (count > 5) {
-//       ret = true;
-//     }
-//   });
-//   return ret;
-// }
-//
-// console.log(selections)
-
-//TIMER
-var count = 0;
-$(".button").on('click', function(){
-   var count = 11;
-   setInterval(function() {
-     count--;
-      if (count >= 0) {
-         span = $(".count");
-         span.innerHTML = count;
-         console.log(count)
-      }
-      if (count === 0) {
-         alert('YOU JUST WENT BOOOOOOM!');
-         clearInterval(count);
-         location.reload("#board");
-         return;
-       }
-       document.getElementById("timer").innerHTML=count + " secs";
-     }, 1000);
-});
-
-//cover div
-//
-// $("#dimScreen").on("click", function() {
-//     $(".hiddenDiv").css({
-//       height: "0px"
-//     }, 1000 ); // how long the animation should be
-//   });
-
-
-// audio
-
-function playHeart() {
-  heartBeat.play();
-}
-
-
-
 });
